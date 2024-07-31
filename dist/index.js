5643646befd6e51ea8071f62ba09f096332e930e
MortallicaXxX <MortallicaXxX@hotmail.com>
2024-07-31 15:58:31 +0000

    Add generated files

--- dist/index.js
+++
@@ -1,2 +0,0 @@
-export { createStorage } from './create-storage.js';
-//# sourceMappingURL=index.js.map
--- src/index.ts
+++
@@ -1,2 +0,0 @@
-export { createStorage } from './create-storage.js';
-export type { Storage , StorageKeys , StorageValues , StateValidator } from './models/storage.js'
